0c0ecf0739204d090be53a1d622f44b9016a77ec
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-21 23:54:14 -0700

    added a triangle noise function that only kind of works vertically

--- perlin_noise.py
+++ perlin_noise.py
@@ -1,6 +1,8 @@
 import random, pygame, math
 
-# random.seed(89243598)
+random.seed(89243598)
+# https://www.desmos.com/calculator/fb4sly23hg
+
 y_scale = 2
 
 def smooth(x):
@@ -33,12 +35,13 @@ class perlin_noise_2d():
 		self.height = height + 1
 
 	def generate_noise(self):
+		# self.points = [[0.3, 1], [0, 0.5]]
 		self.points = []
 		for i in range(self.height):
 			row = [random.uniform(0, 1) for i in range(self.length)]
 			self.points.append(row)
 
-	def noise(self, x, y):
+	def noise_square(self, x, y):
 		whole_x = int(x)
 		whole_y = int(y)
 		x_deci = x - whole_x
@@ -58,6 +61,32 @@ class perlin_noise_2d():
 		interp_x = interp_y1 + smooth(x_deci) * (interp_y2 - interp_y1)
 
 		return (interp_x + interp_y) / 2
+
+	def noise_triangle(self, x, y):
+		whole_x = int(x)
+		whole_y = int(y)
+		x_deci = x - whole_x
+		y_deci = y - whole_y
+		side = x_deci + y_deci
+
+		origin = self.points[whole_y][whole_x]
+		end_x = self.points[whole_y][whole_x + 1]
+		end_y = self.points[whole_y + 1][whole_x]
+		end_xy = self.points[whole_y + 1][whole_x + 1]
+
+		if (side <= 1):
+			interp_x = origin + smooth(side) * (end_x - origin)
+			interp_y = origin + smooth(side) * (end_y - origin)
+
+			return interp_x + (smooth(math.sqrt(2 * (y_deci**2))) / 2) * (interp_y - interp_x)
+
+		else:
+			side = (1 - x_deci) + (1 - y_deci)
+
+			interp_x = end_y + smooth(1 - side) * (end_xy - end_y)
+			interp_y = end_x + smooth(1 - side) * (end_xy - end_x)
+
+			return interp_x + (smooth(math.sqrt(2 * (1 - y_deci**2))) / 2) * (interp_y - interp_x)
 
 
 
@@ -83,23 +112,32 @@ class perlin_noise_2d():
 
 
 
-pn = perlin_noise_2d(85, 45)
+pn = perlin_noise_2d(4, 2)
 pn.generate_noise()
+
+print(pn.noise_triangle(0.5, 0.99))
+print(pn.noise_triangle(0.5, 1.01))
+print()
 
 pygame.init()
 window = pygame.display.set_mode((1800, 900))
 
+for x in range(0, 400, 3):
+	for y in range(0, 200, 3):
+		try:
+			color = pn.noise_triangle(x / 100, y / 100) * 255
+		except:
+			pass
+		for i in range(3):
+			for j in range(3):
+				try:
+					window.set_at((x + i + 50, y + j + 50), (color, color, color))
+				except:
+					print(x / 100, y / 100)
+
+		pygame.display.update()
+
 while True:
-
-	for x in range(0, 1700, 3):
-		for y in range(0, 800, 3):
-			color = pn.noise(x / 20, y / 20) * 255
-			for i in range(3):
-				for j in range(3):
-					window.set_at((x + i + 50, y + j + 50), (color, 255 - color, 255))
-
-			pygame.display.update()
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			quit()
