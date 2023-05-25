247baebaae594afad5fd36b81532d3f7a7a82e7c
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-24 21:02:46 -0700

    OMG IT FREAKING WORKSSSSSSS

--- perlin_noise.py
+++ perlin_noise.py
@@ -1,6 +1,6 @@
 import random, pygame, math
 
-random.seed(89243598)
+# random.seed(89243598)
 # https://www.desmos.com/calculator/fb4sly23hg
 
 y_scale = 2
@@ -35,10 +35,7 @@ class perlin_noise_2d():
 		self.height = height + 1
 
 	def generate_noise(self):
-		self.points = []
-		for i in range(self.height):
-			row = [random.uniform(0, 1) for i in range(self.length)]
-			self.points.append(row)
+		self.points = [[random.uniform(0, 1) for i in range(self.length)] for j in range(self.height)]
 
 	def noise_square(self, x, y):
 		whole_x = int(x)
@@ -66,35 +63,29 @@ class perlin_noise_2d():
 		whole_y = int(y)
 		x_deci = x - whole_x
 		y_deci = y - whole_y
-		# x + y gives us the length of the non hypotenuse sides of a 45 degree triangle
 		tri_side = x_deci + y_deci
 
-		# get the heights of the top left and right, then bottom left and right corners
 		origin = self.points[whole_y][whole_x]
 		end_x = self.points[whole_y][whole_x + 1]
 		end_y = self.points[whole_y + 1][whole_x]
 		end_xy = self.points[whole_y + 1][whole_x + 1]
 
-		# if tri_side is less than 1, the point is in the top left half of the cell
-		# which means we interpolate based on the top and left 1d interpolations
-		# also note this is done at the length of the triangle side so that
-		# when the diagonal interpolation is done it will pass through the point
+		if tri_side == 0:
+			return origin
+
 		if (tri_side <= 1):
 			interp_x = origin + smooth(tri_side) * (end_x - origin)
 			interp_y = origin + smooth(tri_side) * (end_y - origin)
 
-			# lastly interpolate between the two interpolated sides using a diagonal interpolation
-			return interp_x + (smooth(math.sqrt(2 * (y_deci**2))) / math.sqrt(tri_side)) * (interp_y - interp_x)
+			return interp_y + (smooth(math.sqrt(2 * (x_deci**2)) / math.sqrt(2 * (tri_side**2)))) * (interp_x - interp_y)
 
-		# if tri_side is greater than 1, we do the same thing but from the bottom right triangle
-		# so intead of the top and left, we use the bottom and right sides, then interpolate between them as before
 		else:
 			tri_side = (1 - x_deci) + (1 - y_deci)
 
-			interp_x = end_xy + smooth(1 - tri_side) * (end_y - end_xy)
-			interp_y = end_xy + smooth(1 - tri_side) * (end_x - end_xy)
+			interp_x = end_xy + smooth(tri_side) * (end_y - end_xy)
+			interp_y = end_xy + smooth(tri_side) * (end_x - end_xy)
 
-			return interp_y + (smooth(math.sqrt(2 * (1 - y_deci**2))) / math.sqrt(tri_side)) * (interp_x - interp_y)
+			return interp_y + smooth(math.sqrt(2 * (1 - x_deci)**2) / math.sqrt(2 * (tri_side**2))) * (interp_x - interp_y)
 
 
 
@@ -120,26 +111,22 @@ class perlin_noise_2d():
 
 
 
-pn = perlin_noise_2d(4, 2)
+pn = perlin_noise_2d(6, 3)
 pn.generate_noise()
-
-print(pn.noise_triangle(0.49, 0.49))
-print(pn.noise_triangle(0.51, 0.51))
-print()
 
 pygame.init()
 window = pygame.display.set_mode((1800, 900))
 
-for x in range(0, 400, 8):
-	for y in range(0, 200, 8):
+for x in range(0, 1600, 10):
+	for y in range(0, 800, 10):
 		try:
-			color = pn.noise_triangle(x / 100, y / 100) * 255
+			color = pn.noise_triangle(x / 300, y / 300) * 255
 		except:
 			pass
-		for i in range(8):
-			for j in range(8):
+		for i in range(10):
+			for j in range(10):
 				try:
-					window.set_at((x + i + 50, y + j + 50), (color, color, color))
+					window.set_at((x + i + 50, y + j + 50), (255 - color, color, color))
 				except:
 					print(x / 100, y / 100)
 
